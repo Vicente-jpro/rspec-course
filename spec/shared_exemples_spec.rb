03101a1a3134f83070cc21cbc3b0382e8bb70b39
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-02-12 20:37:15 +0100

    shered examples

---
+++ spec/shared_exemples_spec.rb
@@ -0,0 +1,20 @@
+RSpec.shared_examples 'A Ruby object with three elements' do 
+    it 'return the number of items' do 
+        expect(subject.length).to eq(3)
+    end
+end
+
+RSpec.describe Array do 
+    subject {[1, 2, 3]}
+    include_examples 'A Ruby object with three elements'
+end
+
+RSpec.describe String do 
+    subject { 'abc' }
+    include_examples 'A Ruby object with three elements'
+end
+
+RSpec.describe Hash do 
+    subject {{a: 1, b: 2, c: 3}}
+    include_examples 'A Ruby object with three elements'
+end
